81e942a5d19d0a2f0ca843776bb13d8a064bd63b
Martonveghcode <swetymarton123@gmail.com>
2025-10-31 15:53:55 +0100

    started work on chef claude project first comit.added unfinished header element

---
+++ chef-claude/src/header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return(
+        <section className="headerSection">
+            <nav className="HeaderNav">
+                <div className="titleContainer">
+                    <img src="\src\assets\robot-chef-holding-fork-knife-symbolizing-automation-food-industry-chef-robot-holding-fork-knife-kitchen-331778142.png" alt="robot picture" id="robot"/>
+                    <p className="title">Chef claude</p>
+                </div>
+            </nav>
+
+        </section>
+    )
+}
