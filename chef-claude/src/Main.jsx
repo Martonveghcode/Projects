561e911887187c2b08005ec7c09eec0b71141684
Martonveghcode <swetymarton123@gmail.com>
2025-11-02 08:33:38 +0100

    added ingredient rendering using form.not yet capable of using react.usestate()

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,11 +1,32 @@
 export default function Main() {
+    let ingredients = ["Chicken", "Oregano", "Tomatoes","marton"]
+
+    ingredients = ingredients.map((x) => {
+        return(<li key={x}>{x}</li>)
+    })
+
+
+    function handleSubmit(event) {
+        console.log(ingredients)
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+        
+        console.log(ingredients)
+
+
+    }
+
     return(
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. aregano"/>
+            <form className="add-ingredient-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="e.g. aregano" name="ingredient"/>
                 <button>+ ingredients</button>
             </form>
-            <div/>
+            <ul>
+                {ingredients}
+            </ul>
   
         </main>
         
