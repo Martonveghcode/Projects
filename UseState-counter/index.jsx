20d9f998747dceb8d31a9fe622f4973e9ffe4021
Martonveghcode <swetymarton123@gmail.com>
2025-11-07 15:27:50 +0100

    practiced UseState() in react

---
+++ UseState-counter/index.jsx
@@ -0,0 +1,15 @@
+import {createRoot} from "react-dom/client"
+import App from "./src/app"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <>
+        <main>
+            <App/>
+        </main>
+        
+    </>
+    
+
+)
