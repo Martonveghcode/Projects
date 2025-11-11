d676ca9b79c55db359c9380ccde98d1302bf48fe
Martonveghcode <swetymarton123@gmail.com>
2025-11-11 16:08:56 +0100

    started work on a calc project to practice usestate and forms in react

---
+++ calculator/index.jsx
@@ -0,0 +1,11 @@
+import {createRoot} from "react-dom/client"
+
+import Interface from "./src/interface"
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <>
+        <Interface/>
+    
+    </>
+)
---
+++ calculator/src/interface.jsx
@@ -0,0 +1,33 @@
+export default function Interface() {
+    return(
+        <>
+        <main>
+            <header>
+                <h2 className="output-el">
+                    response goes here
+                </h2>
+            </header>
+
+            <section className="keyboard">
+                    <section className="row1">
+                        <button className="1">1</button>
+                        <button className="2">2</button>
+                        <button className="3">3</button>
+                    </section>
+                    
+                    <section className="row2">
+                        <button className="4">4</button>
+                        <button className="5">5</button>
+                        <button className="6">6</button>
+                    </section>
+                    <section className="row3">
+                        <button className="7">7</button>
+                        <button className="8">8</button>
+                        <button className="9">9</button>
+                    </section>
+            </section>
+        </main>
+        
+        </>
+    )
+}
