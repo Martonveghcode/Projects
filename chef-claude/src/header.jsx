e819e2af2c55063647daaf445fb72a8b696c6ba2
Martonveghcode <swetymarton123@gmail.com>
2025-11-14 16:00:07 +0100

    refactored header.jsx and implemented conditiononal rendering

--- chef-claude/src/header.jsx
+++ chef-claude/src/header.jsx
@@ -1,14 +1,8 @@
 export default function Header() {
-    return(
-        <section className="headerSection">
-            <nav className="HeaderNav">
-                <div className="titleContainer">
-                    <img src="\src\assets\robot-chef-holding-fork-knife-symbolizing-automation-food-industry-chef-robot-holding-fork-knife-kitchen-331778142.png" alt="robot picture" id="robot"/>
-                    <h1 className="title">Chef claude</h1>
-                </div>
-
-            </nav>
-            
-        </section>
+    return (
+        <header>
+            <img src="chef-claude\src\assets\robot-chef-holding-fork-knife-symbolizing-automation-food-industry-chef-robot-holding-fork-knife-kitchen-331778142.png"/>
+            <h1>Chef Claude</h1>
+        </header>
     )
 }
