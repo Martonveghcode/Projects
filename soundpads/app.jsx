c8f3d65432e5f6102cffd614c042f346ab50ab13
Martonveghcode <swetymarton123@gmail.com>
2025-11-16 08:42:13 +0100

    almost finished soundpads and learning sharedprops

--- soundpads/app.jsx
+++ soundpads/app.jsx
@@ -1,17 +1,23 @@
 import { useState } from "react"
 import pads from "./pads"
+import Pad from "./pad"
 
 export default function App() {
+    // eslint-disable-next-line no-unused-vars
     const [padsState, setPadsState] = useState(pads)
+    
 
+    function toggle() {
+        console.log("clicked!")
+    }
     return (
         <main>
             <div className="pad-container">
                 {padsState.map(pad => (
-                    <button>
-                        {pad}
-                    </button>
-                ))}
+                    <Pad key={pad.id} color={pad.color} func={toggle} 
+                        
+                    />))}
+                
             </div>
         </main>
     )
