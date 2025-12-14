30783364e8ea417a1692cf72a0a900652927d51a
Martonveghcode <swetymarton123@gmail.com>
2025-12-14 14:05:11 +0100

    updated portfolio

--- portfolio/index.jsx
+++ portfolio/index.jsx
@@ -1,10 +1,12 @@
-import { createRoot } from "react-dom/client"
-import App from "./src/App"
-const root = createRoot(document.getElementById("root"))
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./src/App";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    <>
-        <body><App/></body>
-        
-    </>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
