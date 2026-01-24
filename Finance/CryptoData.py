2fda843423c96a2386cb26577c9627e1700d8496
Martonveghcode <swetymarton123@gmail.com>
2026-01-24 09:56:32 +0100

    crypto data test

---
+++ Finance/CryptoData.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+
+
+# No keys required for crypto data
+client = CryptoHistoricalDataClient()
+
+request_params = CryptoBarsRequest(
+    symbol_or_symbols=["BTC/USD"],
+    timeframe=TimeFrame.Day,
+    start=datetime(2022, 9, 1),
+    end=datetime(2022, 9, 7),
+)
+
+# Retrieve daily bars for Bitcoin in a DataFrame and printing it
+btc_bars = client.get_crypto_bars(request_params)
+
+# Convert to dataframe
+print(btc_bars.df)
